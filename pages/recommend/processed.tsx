a1297b856b65e7603b6ad2a763363fa0dde2658d
Marc Capde <capasc5@gmail.com>
2022-05-01 04:36:42 +0200

    furula

--- pages/recommend/processed.tsx
+++ pages/recommend/processed.tsx
@@ -15,11 +15,13 @@ const Processed: NextPage = () => {
 
   const image_url = 'https://i.imgur.com/JynJP5l.png'
 
+  const [tabActivo,setActivo] = useState<any>(0);
 
   const cositas = async () => {
     const response = await fetch(`http://localhost:3000/api/procesa_datos?image_url=${image_url}`);
     const data = await response.json();
-    setImageData(data);
+    setImageData(data.falta);
+    setImpData(data.suggestionsMejora);
     console.log(`http://localhost:3000/api/procesa_datos?image_url=${image_url}`)
     setIsLoading(false);
   }
@@ -38,10 +40,9 @@ const Processed: NextPage = () => {
       ) : (
         <>
           <ImageFrame image_url={image_url}/>
-          <Tabs  variant="pills" orientation='horizontal'>
-            <Tabs.Tab label="Mising"> {imageData != null ? <Taula data={imageData}/> : null} </Tabs.Tab>
-            <Tabs.Tab label="Improvements"> {imageData != null ? <Taula data={imageData}/> : null} </Tabs.Tab>
-
+          <Tabs orientation='horizontal' >
+            <Tabs.Tab onClick={() => setActivo('missing')} label="Missing" style={{ color: 'black' }}> {imageData != null ? <Taula data={imageData}/> : null} </Tabs.Tab>
+            <Tabs.Tab onClick={() => setActivo('improve')} label="Improvements" style={{ color: 'black' }}> {impData != null ? <Taula data={impData}/> : null} </Tabs.Tab>
           </Tabs>
           
         </>
